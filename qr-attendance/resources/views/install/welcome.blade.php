1a7cc45a00cdafa31ef1905bc7bde7f0fe72b53d
ikadityaa <ikadityaa@gmail.com>
2025-08-11 19:54:37 +0700

    Delete qr-attendance directory

--- qr-attendance/resources/views/install/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<x-guest-layout>
-    <div class="mb-4 text-center">
-        <h1 class="text-2xl font-bold">Welcome to QR Attendance Installer</h1>
-        <p class="text-gray-600 mt-2">This wizard will guide you through the initial setup.</p>
-    </div>
-    <div class="mt-6 text-center">
-        <a href="{{ route('install.requirements') }}" class="px-4 py-2 bg-indigo-600 text-white rounded">Start Installation</a>
-    </div>
-</x-guest-layout>
